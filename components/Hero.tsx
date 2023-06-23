fc20e7d1d8d4081061c6ed0233aa815b9c708100
+ <+>
2023-06-23 18:53:15 +0300

    adding image overlay

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,8 +28,9 @@ const Hero = () => {
             <Image 
               src='/hero.png' alt='hero' fill className='object-contain'
             />
+            </div>
             <div className='hero_image-overlay' />
-          </div>
+      
         </div>
     </div>
   )
