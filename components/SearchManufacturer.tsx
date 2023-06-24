06b226e924c47de87ced46c299f5fde10def3f60
+ <+>
2023-06-24 14:49:45 +0300

    showing active or selected option

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -60,7 +60,27 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     key={item}
                     value={item}
                     className={({ active }) => `relative search_manufacturer-option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
-                  >{item}</Combobox.Option>
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          > 
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
                 ))
               )}
             </Combobox.Options>
