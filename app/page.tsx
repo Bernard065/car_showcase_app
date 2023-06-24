ce1a0f92b7f9ed506dc20a1465cf914e2f2f7560
+ <+>
2023-06-24 04:11:46 +0300

    creating the car catalogue section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CustomFilter, SearchBar } from '@/components'
 import Hero from '@/components/Hero'
 import Image from 'next/image'
 
@@ -5,6 +6,22 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home_text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore your favorite cars</p>
+        </div>
+
+        <div className='home_filters'>
+          <SearchBar />
+
+          <div className='home_filter-container'>
+            <CustomFilter />
+            <CustomFilter />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
